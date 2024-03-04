5684d6942d00c4eff620014b36e42c9faa88c97d
EJ Jung <ejung@cs.usfca.edu>
2024-03-04 12:20:42 -0800

    adding ->

--- 0226/structdemo.c
+++ 0226/structdemo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct point {
     float x;
@@ -10,4 +11,10 @@ int main(int argc, char* argv[]) {
     p.x = 1.0;
     p.y = 0.1;
     printf("%f, %f\n", p.x, p.y);
+
+    struct point* q;
+    q = (struct point*) malloc(sizeof(struct point));
+    q->x = 2.0;
+    q->y = 0.2;
+    printf("%f, %f\n", q->x, q->y);
 }
