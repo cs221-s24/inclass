537ffd45540191994f4bb843b29e46f025348654
EJ Jung <ejung@cs.usfca.edu>
2024-03-04 12:29:00 -0800

    adding ->

--- 0226/typedefdemo.c
+++ 0226/typedefdemo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct point {
     float x;
@@ -10,4 +11,9 @@ int main(int argc, char* argv[]) {
     p.x = 1.0;
     p.y = 0.1;
     printf("%f, %f\n", p.x, p.y);
+
+    point* q = (point*) malloc(sizeof(point));
+    q->x = 2.0;
+    q->y = 0.2;
+    printf("%f, %f\n", q->x, q->y);
 }
