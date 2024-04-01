0e238af31f176674dd6deeee3fd07fdb1883a9b6
EJ Jung <ejung@cs.usfca.edu>
2024-03-31 23:55:20 -0700

    0401

--- 0401/realloctest.c
+++ 0401/realloctest.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void printList(int* list, int length) {
 	for (int i=0; i<length; i++) {
@@ -10,8 +11,24 @@ void printList(int* list, int length) {
 
 int main(int argc, char* argv[]) {
 	int nums[] = {1, 2, 3, 4, 5};
-	int* nums2 = realloc(nums, 6);
-	nums2[5] = 6;
-	printList(nums2, 6);
+	int* nums2 = (int*) malloc(sizeof(int)*5);
+    memset(nums2, 0, sizeof(int)*5);
+    for (int i=0; i<5; i++) {
+    	nums2[i] = i+1;
+    }
+    printf("nums: ");
+    printList(nums, 5);
+    printf("nums2: ");
+    printList(nums2, 5);
+
+    // nums = realloc(nums, 6);
+    nums2 = (int*) realloc(nums2, 6);
+
+    nums2[5] = 6;
+    printList(nums2, 6);
+
+    nums2 = (int*) realloc(nums2, 4);
+    printList(nums2, 4);
+    printList(nums2, 6);
 
 }
